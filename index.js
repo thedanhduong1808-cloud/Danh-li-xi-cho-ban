31caf7cc8dd307fa93605c8c88b4949cd356d9ac
thedanhduong1808-cloud <thedanhduong1808@gmail.com>
2026-02-12 19:53:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("result").innerText =
+    "🎊 Bạn trúng: " + prizes[prizeIndex];
+const prize = prizes[prizeIndex];
+
+document.getElementById("result").innerText =
+    "🎊 Bạn trúng: " + prize;
+
+saveResult(prize);
